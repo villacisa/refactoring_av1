c489155277988b1a3a13741eec1393fb11fb5b68
macbookair <villacisajd@gmail.com>
2021-05-28 18:10:18 +0200

    Cinquè commit on afegim la variable local EUROS_PER_UNITAT_DE_COST

--- refactoring_av/src/refactoring_av/Client.java
+++ refactoring_av/src/refactoring_av/Client.java
@@ -8,6 +8,8 @@ public class Client {
     private String nom;
     private String telefon;
     private ArrayList<Lloguer> lloguers;
+    private static final double EUROS_PER_UNITAT_DE_COST = 30;
+
     
     public Client(String nif, String nom, String telefon) {
         this.nif = nif;
@@ -62,8 +64,8 @@ public class Client {
                 lloguer.getVehicle().getMarca() +
                 " " +
                 lloguer.getVehicle().getModel() + ": " +
-                (lloguer.quantitat() * 30) + "€" + "\n";
-            total += lloguer.quantitat() * 30;
+                (lloguer.quantitat() * EUROS_PER_UNITAT_DE_COST) + "€" + "\n";
+            total += lloguer.quantitat() * EUROS_PER_UNITAT_DE_COST;
         }
 
         // afegeix informació final
